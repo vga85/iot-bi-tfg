dda7b80894ed1450ff993ac5fc41c20a7f56579c
vicente.gonzalez <vicente.gonzalez@netcentric.biz>
2020-11-06 15:32:01 +0100

    Changing variables

--- dht_cli.py
+++ dht_cli.py
@@ -1,42 +1,30 @@
-#!/usr/bin/python
+#  Adafruit DHT library:
+#  https://github.com/adafruit/Adafruit_Python_DHT
+#
+#  Based on examples by Tony DiCola
+#
+# Author : Vicente Gonzalez
+# Date   : 06/11/2020
+#
+# 
 
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-#import needed packages
+#import Adafruit Library
 import Adafruit_DHT
+import time
 
-# Setting the sensor used.
+# We define the type of sensor. Change to DHT22 if you use this type of DHT-sensor.
 sensor = Adafruit_DHT.DHT11
-
 # Where OUT pin is connected to Raspberry Pi
-# connected to GPIO17.
-pin = 17
+gpio = 17
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# print humidity and temperature values
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get values. Try again!')
+while True:
+    #Reading the sensor
+    humidity, temperature = Adafruit_DHT.read(sensor, gpio)
+    #if we get a reading value from the sensor
+    if humidity is not None and temperature is not None:
+    #printing the results on the screen
+        print("Temp={0:0.1f}C  Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Sensor failure. Check wiring.");
+    #Receiving values every 60 seconds
+    time.sleep(60);
--- dht_powerbi.py
+++ dht_powerbi.py
@@ -1,34 +1,43 @@
-"""
-Python sample for Raspberry Pi which reads temperature and humidity values from
-a DHT11 sensor, and sends that data to Power BI for use in a streaming dataset.
-"""
+#  Read humidity and temperature values from DHTxx sensor and
+#  send data to Power BI streaming dataset
+#
+#  Adafruit DHT library:
+#  https://github.com/adafruit/Adafruit_Python_DHT
+#
+#  Based on examples by Sirui Sun
+#
+# Author : Vicente Gonzalez
+# Date   : 06/11/2020
+#
+# 
 
+# Importing needing libraries
 import urllib3, time
 from datetime import datetime
-import Adafruit_DHT as dht
+import Adafruit_DHT
 
 # type of sensor that we're using
-SENSOR = dht.DHT11  
+sensor = Adafruit_DHT.DHT11 
 
 # pin which reads the temperature and humidity from sensor GPIO port
-PIN = 17         
+gpio = 17         
 
 # REST API endpoint, given to you when you create an API streaming dataset
 # Will be of the format: https://api.powerbi.com/beta/<tenant id>/datasets/< dataset id>/rows?key=<key id>
-REST_API_URL = 'https://api.powerbi.com/beta/aec762e4-3d54-495e-a8fe-4287dce6fe69/datasets/6f2f8db8-6aff-420e-aa3b-a255debe33d8/rows?noSignUpCheck=1&key=ws0lnS9yrp%2F%2FlMGsVKtizZgl%2BagEM%2Bzar2c21fWkkJewESOp4iffwp03LvzlCFNZWUMYFRp0SDictwNu%2FRUJNg%3D%3D'
+REST_API_URL = 'https://api.powerbi.com/beta/aec762e4-3d54-495e-a8fe-4287dce6fe69/datasets/3f8a8652-9348-4a0e-8269-657107031bb0/rows?noSignUpCheck=1&key=kNRO6B8sLBvjAm1VIyasCWutvy83omGZGUQdBHUK%2B0CqoVnoXIrioOMtrewuARyfq8xCB4n6YAu8Iqhy8Hceog%3D%3D'
 
 # Gather temperature and sensor data and push to Power BI REST API
 while True:
     try:
         # read and print humidity and temperature values from sensor
-        humidity,temp = dht.read_retry(SENSOR, PIN)
-        #print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp, humidity)
+        humidity,temperature = Adafruit_DHT.read_retry(sensor, gpio)
+        #print ("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
         
         # ensure that timestamp string is formatted properly
         now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S%Z")
     
         # data that we're sending to Power BI REST API
-        data = '[{{ "timestamp": "{0}", "temperature": {1:0.1f}, "humidity": {2:0.1f} }}]'.format(now, temp, humidity)
+        data = '[{{ "timestamp": "{0}", "temperature": {1:0.1f}, "humidity": {2:0.1f} }}]'.format(now, temperature, humidity)
         print (data)
         # make HTTP POST request to Power BI REST API
         http = urllib3.PoolManager()
